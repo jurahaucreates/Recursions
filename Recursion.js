e09ca2e828d2e53968da3b1eec96a90888cf2ce9
jurahaucreates <44047192+jurahaucreates@users.noreply.github.com>
2020-07-02 11:38:18 -0400

    Update Recursion.js

--- Recursion.js
+++ Recursion.js
@@ -1,1 +1,17 @@
 hello
+
+
+//factorial:
+
+function factorial(n) {
+  if( n=== 0); return 1;
+  return(n) * factorial(n-1)
+}
+
+//integers in a range in an array
+
+function rangeOfNumbers(startNum, endNum) {
+  return startNum === endNum
+    ? [startNum]
+    : rangeOfNumbers(startNum, endNum - 1).concat(endNum);
+}
