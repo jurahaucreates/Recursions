d0b6f47191cc1b8339cd617c5947998730aea7d3
jurahaucreates <44047192+jurahaucreates@users.noreply.github.com>
2020-08-12 19:00:38 -0400

    Update Recursion.js

--- Recursion.js
+++ Recursion.js
@@ -7,6 +7,9 @@ function factorial(n) {
   if( n=== 0); return 1;
   return(n) * factorial(n-1)
 }
+factorial(5)
+
+
 
 //integers in a range in an array
 
@@ -14,4 +17,36 @@ function rangeOfNumbers(startNum, endNum) {
   return startNum === endNum
     ? [startNum]
     : rangeOfNumbers(startNum, endNum - 1).concat(endNum);
+}
+
+//find longest word in a string
+function findLongestWordLength(str) {
+  var words = str.split(' ');
+  var maxLength = 0;
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+//or
+
+function findLongestWord(str) {
+  var longestWord = str.split(' ').reduce(function(longest, currentWord) {
+    return currentWord.length > longest.length ? currentWord : longest;
+  }, "");
+  return longestWord.length;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+//return largest numbers in an array
+function largestOfFour(arr, finalArr = []) {
+  return !arr.length
+    ? finalArr
+    : largestOfFour(arr.slice(1), finalArr.concat(Math.max(...arr[0])))
 }
